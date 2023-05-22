e0866d60e164d62146815cc109fe74f3e9522e18
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-22 09:47:39 -0400

    Update Main.Console.Scratchpad.cpp

--- Main.Console.Scratchpad.cpp
+++ Main.Console.Scratchpad.cpp
@@ -1,25 +1,89 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <Euclid.h>
 //#include <Euclid.TestCase.246.h>
-#include <Euclid.16.Boost.Support.No.Strings.h>
+//#include <Euclid.16.Boost.Support.No.Strings.h>
 
 int main()
 {
-    Euclid_Prover::__stdtracein__("Main");
+	std::vector<
+		std::vector<
+		std::vector<
+		std::vector<
+		std::string>>>>
 
-    const auto start_time_chrono = std::chrono::high_resolution_clock::now();
+		// Instantiate ProofStep4DStdStrVec[proof][step][lhs/rhs][token]
+		ProofStep4DStdStrVec;
 
-    std::cout << "Working..." << std::endl;
+	std::vector<
+		std::vector<
+		std::string>>
 
-    Euclid_Prover::Prove();
+		// Instantiate AxiomCommitLogStdStrVec[proof][step]
+		AxiomCommitLogStdStrVec;
+
+	using EuclidProverClass =
+
+		Euclid_Prover::EuclidProver<
+		Euclid_Prover::BracketType::CurlyBraces>;
+
+	EuclidProverClass Euclid;
+
+	Euclid.Axioms
+	(
+		{
+			// Axiom_00
+			{
+				{ "1", "+", "1" },  // lhs
+				{ "2" } // rhs
+			},
+
+			// Axiom_01
+		   {
+				{ "2", "+", "2" }, // lhs
+				{ "4" } // rhs
+			}
+		}
+	);
+
+	Euclid.Prove
+	(
+		{
+			{ "1", "+", "1", "+", "1", "+", "1" }, // lhs
+			{ "4" } // rhs
+		},
+
+		ProofStep4DStdStrVec,
+		AxiomCommitLogStdStrVec
+	);
+
+	const auto start_time_chrono = std::chrono::high_resolution_clock::now();
+
+	std::cout << "Working..." << std::endl;
+
+	while (!Euclid.StatusReadyFlag)
+	{
+		std::this_thread::yield();
+	}
+
+	if (Euclid.ProofFoundFlag)
+	{
+		std::cout << "Proof Found. (QED)" << std::endl;
+		Euclid.PrintPath(ProofStep4DStdStrVec);
+		Euclid.PrintPath(AxiomCommitLogStdStrVec);
+	} else if (ProofStep4DStdStrVec.size()) {
+		std::cout << "Partial Proof Found." << std::endl;
+		Euclid.PrintPath(ProofStep4DStdStrVec);
+		Euclid.PrintPath(AxiomCommitLogStdStrVec);
+	} else {
+		std::cout << "No Proof Found." << std::endl;
+	}
 
     std::cout << std::endl;
     const auto end_time_chrono = std::chrono::high_resolution_clock::now();
     const auto duration_chrono = std::chrono::duration_cast<std::chrono::nanoseconds>( end_time_chrono - start_time_chrono ).count();
     std::cout << "Total Duration (nanoseconds): " << duration_chrono << std::endl;
-
-    Euclid_Prover::__stdtraceout__("Main");
 
     std::string Hold_for_user_input{};
     std::cin >> Hold_for_user_input;
